8593efca8acd7800566a418dcbf7349fb88e2395
Jamey Kirk <JameyKirk@gmail.com>
2022-01-14 23:23:50 +0900

    Initial commit

---
+++ Assignment_Ch03-01_Kirk.py
@@ -0,0 +1,17 @@
+num1, num2, num3 = input("Please enter three numbers:\n").split()
+print("\n")
+print("The three integer numbers in ascending order:")
+if num1 <= num2 and num2 <= num3:
+    print(num1, num2, num3)
+elif num1 <= num3 and num3 <= num2:
+    print(num1, num3, num2)
+    
+elif num2 <= num3 and num3 <= num1:
+    print(num2, num3, num1)
+elif num2 <= num1 and num1 <= num3:
+    print(num2, num1, num3)
+    
+elif num3 <= num1 and num1 <= num2:
+    print(num3, num1, num2)
+else:
+    print(num3, num2, num1)
